9e2df3fbec59bdf6e861bdeeed02ba00a54a37b3
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-08 23:42:55 -0500

    Config080722

---
+++ EjemploSIST/ObregonDSI/ObregonDSI/Models/RegistroDoc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ObregonDSI.Models
+{
+    public class RegistroDoc
+    {
+        [Key]
+        public int IdRegistroDoc { get; set; }
+
+        [Display(Name = "Proveedor")]
+        [MaxLength(100, ErrorMessage = "El campo no debe de tener mas de 100 caracteres")]
+        public string Proveedor { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
