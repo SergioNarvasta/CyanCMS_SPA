13c6df8b4ab5c97a62c6bcd421a915934fdfe152
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-13 00:14:48 -0500

    DbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,14 +8,17 @@ namespace SistemaAcad.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
         {
 
             optionsBuilder.UseSqlServer("Server=DESKTOP-H70G0ED\\SQLEXPRESS;Database=BDSistemaAcad;Trusted_Connection=true; MultipleActiveResultSets=true;");
         }
+
     }
 }
