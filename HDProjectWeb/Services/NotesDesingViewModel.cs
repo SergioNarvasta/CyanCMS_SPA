6a06d2b38866c5f66d84ea11f6317f0a7862121d
Sergio Narvasta <sergio.jn024@gmail.com>
2022-11-30 18:31:26 -0500

    Bootstrap Implementation

---
+++ HDProjectWeb/Services/NotesDesingViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HDProjectWeb.Services
+{
+    public class NotesDesingViewModel
+    {
+        /* 
+          
+          Clases de Bootstrap para diseño de interfaces
+           d-flex flex-row    ->> alineamiento horizontal
+           me-5 ms-2 ms-xl-5  ->> Margen e(end) left, s(start) right
+           form-control-sm    ->> Tamaño de objeto small
+           col-sm-4           ->> Ancho de objeto small
+
+         */
+    }
+}
