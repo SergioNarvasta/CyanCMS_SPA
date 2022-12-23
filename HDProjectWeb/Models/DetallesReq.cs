ed4e8eb73f46e30a0cd98c8b7fb153c91d63b726
Sergio Narvasta <sergio.jn024@gmail.com>
2022-12-23 18:39:29 -0500

    Dev Details prd

---
+++ HDProjectWeb/Models/DetallesReq.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HDProjectWeb.Models
+{
+    public class DetallesReq
+    {
+        //Model for details products
+    
+        public string Item { get; set; }    
+        public string Descri { get; set; }
+        public string Codigo { get; set; }
+        public string Glosa { get; set; }
+        public string Unidad { get; set; }
+        public string Cantidad { get; set; }
+        public string Codprov { get; set; }
+        public string Nomprov { get; set; }
+
+
+    }
+}
