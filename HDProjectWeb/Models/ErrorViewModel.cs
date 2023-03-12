252579ae17c44711274e4f1899ff14ce84cadae2
SergioNarvasta <sergio.jn024@gmail.com>
2023-03-11 22:45:57 -0500

    Desarrollo de Componente SliderMain

--- HDProjectWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HDProjectWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
