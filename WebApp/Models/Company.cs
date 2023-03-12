6e8449c7c7585969a3516aede7770805e10ed135
SergioNarvasta <sergio.jn024@gmail.com>
2023-03-12 00:35:06 -0500

    Company Class

---
+++ WebApp/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Site.Models
+{
+	public class Company
+	{
+		[BsonId]
+		public ObjectId Company_Id { get; set; }
+	}
+}
