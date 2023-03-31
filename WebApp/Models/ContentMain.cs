debec99d79e1f5c268a9f22aeac5c7165708c12d
SergioNarvasta <sergio.jn024@gmail.com>
2023-03-30 23:18:07 -0500

    ContentMainViewComponent Design

--- WebApp/Models/ContentMain.cs
+++ WebApp/Models/ContentMain.cs
@@ -1,10 +1,12 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using Site.Models.Comunes;
 
 namespace Site.Models
 {
     public class ContentMain : FileUnit
     {
+        [BsonId]
         public ObjectId ContentMain_Id { get; set; }
 
         public string ContentMain_Pk { get; set; }
@@ -12,11 +14,11 @@ namespace Site.Models
         public string ContentMain_Titulo { get; set; }
 
         public string ContentMain_Descripcion { get; set; }
+        public string ContentMain_Contenido { get; set; }
 
         public int ContentMain_Estado { get; set; }
 
         public int ContentMain_Orden { get; set; }
-
-        public string ContentMain_UsuarioPk { get; set; }
+        public string Company_Pk { get; set; }
     }
 }
