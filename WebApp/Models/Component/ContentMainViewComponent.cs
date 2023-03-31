26ebf07ab182b78146c101f78f83b272faac2bc8
SergioNarvasta <sergio.jn024@gmail.com>
2023-03-30 22:58:20 -0500

    ContentMainViewComponent

--- WebApp/Models/Component/ContentMainViewComponent.cs
+++ WebApp/Models/Component/ContentMainViewComponent.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Site.Interfaces;
-using Site.Models;
 
 namespace HDProjectWeb.Models.Detalles
 {
@@ -9,14 +8,16 @@ namespace HDProjectWeb.Models.Detalles
 	{
         private readonly IContentMainRepository _repository;
         private readonly IConfiguration _configuration;
+
         public ContentMainViewComponent(IContentMainRepository repository, IConfiguration configuration)
         {
             _repository = repository;
             _configuration = configuration;
         }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var company_Pk = _configuration[""];
+            var company_Pk = _configuration["Company_Pk"];
             var list = await _repository.GetByCompanyPk(company_Pk);
             return View(list);
         }
