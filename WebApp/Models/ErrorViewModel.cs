46295824381adc87795f234d6af9dc9881e928a6
SergioNarvasta <sergio.jn024@gmail.com>
2023-05-28 23:00:08 -0500

    Create Models

--- WebApp/Models/ErrorViewModel.cs
+++ WebApp/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace Site.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
--- WebApp/Models/SiteMenuOptions.cs
+++ WebApp/Models/SiteMenuOptions.cs
@@ -1,9 +1,11 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Site.Models
 {
 	public class SiteMenuOptions
 	{
+		[BsonId]
 		public ObjectId _id { get; set; }
 		public string MenuOptions_Nombre { get; set; }
         public int MenuOptions_Estado { get; set; }
